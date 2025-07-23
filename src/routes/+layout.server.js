a8b1803cbc5bd39e1b9d097d6f27659c2e7b83eb
Lawrrr <lawrboni@gmail.com>
2025-07-24 00:21:51 +0800

    Feature: Navbar

---
+++ src/routes/+layout.server.js
@@ -0,0 +1,12 @@
+export function load() {
+  return {
+    menu: [
+      {text: 'Studio', url: 'https://www.feeldx.com/studio'},
+      {text: 'Project Control', url: 'https://www.feeldx.com/project-control'},
+      {text: 'Services', url: 'https://www.feeldx.com/services'},
+      {text: 'Technology', url: 'https://www.feeldx.com/technology'},
+      {text: 'Our Work', url: 'https://www.feeldx.com/our-work'},
+      {text: 'Contact Us', url: 'https://www.feeldx.com/contact-us'},
+    ]
+  }
+}
