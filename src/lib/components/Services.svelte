5a348701401fed0588031f82ef71937655a88ff2
Lawrrr <lawrboni@gmail.com>
2025-07-24 22:23:33 +0800

    Feature: Content Sections

---
+++ src/lib/components/Services.svelte
@@ -0,0 +1,181 @@
+<div class="mx-4 grid grid-cols-1 gap-10 py-4">
+  <div class="bg-[#F5F7F2] px-4 py-8">
+    <div>
+      <p class="text-2xl text-[#86906C] font-bold uppercase tracking-wide">
+        Our Capabilities
+      </p>
+    </div>
+    <div class="pt-4">
+      <p>
+        Our team specialises in leveraging storytelling to transform intricate
+        concepts. We skillfully uncover the underlying purpose of each project
+        and translate it into compelling visual content that resonates
+        personally with each audience. Whether embarking on a large-scale
+        construction project, enhancing existing infrastructure, or initiating
+        an entirely new endeavor, our approach accommodates projects of all
+        sizes and complexities across diverse industries.
+      </p>
+    </div>
+    <div class="pt-4">
+      <p
+        class="uppercase tracking-wide font-semibold pb-2 text-[#86906C] text-[1.3em]"
+      >
+        Services
+      </p>
+      <div class="grid md:grid-cols-3 gap-4">
+        <div class="flex flex-col">
+          <div>
+            <p class="text-[#86906C] font-semibold tracking-wide">
+              Visualisation
+            </p>
+          </div>
+          <div class="pt-2">
+            <ul>
+              <li>Content Strategy</li>
+              <li>Story Telling</li>
+              <li>3D Visualisation</li>
+              <li>BIM 3D, 4D, 5D</li>
+              <li>Multimedia Design</li>
+            </ul>
+          </div>
+          <div class="pt-6 mt-auto">
+            <a
+              href="https://www.feeldx.com/services#visualisations"
+              class="btn tracking-wide uppercase text-sm bg-[#86906C] text-white"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <div>
+            <p class="text-[#86906C] font-semibold tracking-wide">
+              Project Control
+            </p>
+          </div>
+          <div class="pt-2">
+            <ul>
+              <li>Construction Life Cycle Management</li>
+              <li>Project Control & Project Management</li>
+              <li>Tender Programmes</li>
+              <li>Construction Programme</li>
+              <li>Project Forensic Analysis (NOD/EOT Claims & Scheduling)</li>
+            </ul>
+          </div>
+          <div class="pt-6 mt-auto">
+            <a
+              href="https://www.feeldx.com/project-control"
+              class="btn tracking-wide uppercase text-sm bg-[#86906C] text-white"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <div>
+            <p class="text-[#86906C] font-semibold tracking-wide">Education</p>
+          </div>
+          <div class="pt-2">
+            <ul>
+              <li>Education & Training</li>
+            </ul>
+          </div>
+          <div class="pt-6 mt-auto">
+            <a
+              href="https://www.feeldx.com/services#education-and-training"
+              class="btn tracking-wide uppercase text-sm bg-[#86906C] text-white"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bg-[#F5F7F2] px-4 py-8">
+    <div>
+      <p class="text-2xl text-[#86906C] font-bold uppercase tracking-wide">
+        Our Technology
+      </p>
+    </div>
+    <div class="pt-4">
+      <p>
+        Our processes and solutions allow for the intelligent integration of
+        several technologies, including extended reality, 3D Visualisations, 4D
+        & 5D BIM, business intelligence, smart analytics and specialist
+        construction planning.
+      </p>
+    </div>
+    <div class="pt-4">
+      <p class="uppercase font-semibold pb-2 text-[#86906C]">Technology</p>
+      <ul class="grid md:grid-cols-2 gap-2">
+        <li>
+          Artificial Reality (AR), Virtual Reality (VR) & Mixed Reality (MR)
+        </li>
+        <li>High Resolution Renders</li>
+        <li>Flythrough</li>
+        <li>Custom Animations</li>
+        <li>3D Modelling</li>
+        <li>3D & 4D Visualisation and Project Control</li>
+        <li>5D BIM Planning</li>
+      </ul>
+    </div>
+    <div class="pt-4 items-center text-center">
+      <a
+        class="btn py-4 px-8 uppercase tracking-wide bg-[#86906C] text-white"
+        href="https://www.feeldx.com/technology"
+      >
+        Read More
+      </a>
+    </div>
+  </div>
+  <div class="bg-[#F5F7F2] px-4 py-8">
+    <div>
+      <p class="text-2xl text-[#86906C] font-bold uppercase tracking-wide">
+        Our Work
+      </p>
+    </div>
+    <div class="pt-4">
+      <p>
+        We are committed to enhancing user experiences, exploring innovative
+        audience engagement methods, and maximising production efficiency by
+        adopting cutting-edge creative technologies. Our design and development
+        of interactive applications and visualisation tools are driven by a
+        research-centric approach, ensuring they enhance the effectiveness of
+        our communication methods.
+      </p>
+      <p>
+        Our solutions have powered important and impressive projects across a
+        diverse range of industries.
+      </p>
+    </div>
+    <div class="grid md:grid-cols-3 gap-8 py-4">
+      <div>
+        <img
+          src="https://www.feeldx.com/uploads/projects/Mj6YC4Y7bqvt1raxwo8Htm6GQRMHyDaLzGC2JElg.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          src="https://www.feeldx.com/uploads/projects/o3FA2DxyErmj8TzCBZFquEIClFxDnmgEeqaLvXec.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          src="https://www.feeldx.com/uploads/projects/ljqIjL95DTBOcojL2b7ONkEx1tPqWthDTeH1koHJ.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+    <div class="pt-4 items-center text-center">
+      <a
+        class="btn py-4 px-8 uppercase tracking-wide bg-[#86906C] text-white"
+        href="https://www.feeldx.com/our-work"
+      >
+        View our Work
+      </a>
+    </div>
+  </div>
+</div>
