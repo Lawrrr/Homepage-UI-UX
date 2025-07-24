af665a7a7b188b25c05aebee95b4fd521f185155
Lawrrr <lawrboni@gmail.com>
2025-07-24 23:47:35 +0800

    Feature: Hero Section

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,23 @@
+<div
+  class="hero min-h-[55vh]"
+  style="background-image: url(https://www.feeldx.com/uploads/projects/fJ7IDzJpMQsOTL5MYztGHgymcEWZxML3CXAtcYf5.jpg);"
+>
+  <div class="hero-overlay"></div>
+  <div class="hero-content text-neutral-content text-center">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold tracking-wide">
+        Plan. Visualise. Transform.
+      </h1>
+      <p class="mb-5">
+        We push beyond visual representation, integrating and connecting with
+        your project's requirements, bringing them to life and enhancing clarity
+        to create a truly immersive experience.
+      </p>
+      <a
+        href="https://www.feeldx.com/"
+        class="btn bg-[#86906C] uppercase tracking-wide text-white"
+        >Get Started</a
+      >
+    </div>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script>
   import Services from "$lib/components/Services.svelte";
   import Testimonials from "$lib/components/Testimonials.svelte";
+  import HeroSection from "$lib/components/HeroSection.svelte";
 
   const { data } = $props();
   const { testimonials } = data;
 </script>
 
-<div class="container mx-auto">
-  <Services />
-  <Testimonials {testimonials} />
+<div>
+  <HeroSection />
+  <div class="container mx-auto">
+    <Services />
+    <Testimonials {testimonials} />
+  </div>
 </div>
